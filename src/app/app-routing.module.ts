ca76300946c54140643a0328a30de5686c0f78bb
Renato-napoli <renato.napoli75@gmail.com>
2024-11-27 12:26:12 -0300

    Estrutura do componente Card, pagina principal e rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaCardsComponent } from './pages/lista-cards/lista-cards.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListaCardsComponent,
+    title: 'Lista de cards'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaCardsComponent } from './pages/lista-cards/lista-cards.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaCardsComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
