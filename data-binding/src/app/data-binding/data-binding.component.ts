0a4452bd51d4462f54374bdc56ff02886457a164
Renato-napoli <renato.napoli75@gmail.com>
2024-11-29 11:18:20 -0300

    event-binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -11,11 +11,32 @@ export class DataBindingComponent {
   urlImagem: string = 'https://m.media-amazon.com/images/I/8114U+3cgcL._AC_UF894,1000_QL80_.jpg'
   urlImagem2: string = 'https://m.media-amazon.com/images/I/71xYKz7LmhL._AC_UF894,1000_QL80_.jpg'
 
+  valorAtual: string = ''
+  valorEnter: string = ''
+
+  mouseOver: boolean = false
+
   getValor() {
     return 1
   }
 
   getCurtirCurso() {
     return true
+  }
+
+  botaoClicado() {
+    alert('Clicou no botão')
+  }
+
+  OnKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value
+  }
+
+  salvarValor(valor: any) {
+    this.valorEnter = valor
+  }
+
+  onMouseOver() {
+    this.mouseOver = !this.mouseOver
   }
 }
