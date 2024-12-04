447f0a8ef24ae03b311dee49cd1f8f9f877170e1
Renato-napoli <renato.napoli75@gmail.com>
2024-12-04 18:18:30 -0300

    Retirando importacao desnecessaria

--- servicos/src/app/adicionar-curso/adicionar-curso.module.ts
+++ servicos/src/app/adicionar-curso/adicionar-curso.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AdicionarCursoComponent } from './adicionar-curso.component';
 import { CommonModule } from '@angular/common';
 import { ReceberCursoCriadoComponent } from '../receber-curso-criado/receber-curso-criado.component';
