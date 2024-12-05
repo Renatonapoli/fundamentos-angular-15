f28acdd568de44ae09b829d1965f368380dde3ab
Renato-napoli <renato.napoli75@gmail.com>
2024-12-05 15:45:29 -0300

    Configurando rotas simples e RouterLink: definindo rotas no template

---
+++ rotas/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'cursos',
+    component: CursosComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
