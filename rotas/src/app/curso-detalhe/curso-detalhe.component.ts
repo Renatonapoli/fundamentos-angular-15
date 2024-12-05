c8e9a3f57625579027bb99a05d5d01a5ff5d6400
Renato-napoli <renato.napoli75@gmail.com>
2024-12-05 17:39:48 -0300

    Escutando mudanças nos parâmetros de roteamento

--- rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,15 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
   templateUrl: './curso-detalhe.component.html',
   styleUrls: ['./curso-detalhe.component.css']
 })
-export class CursoDetalheComponent {
+export class CursoDetalheComponent implements OnInit {
   id: string = ''
+
+  inscricao: Subscription | undefined
 
   constructor(private idRouter: ActivatedRoute) {
    this.id = this.idRouter.snapshot.params['id']
+   console.log(idRouter)
+  }
+
+  ngOnInit() {
+    this.inscricao = this.idRouter.params.subscribe(params => {
+      this.id = params['id']
+    })
+  }
+
+  ngOnDestroy() {
+    this.inscricao?.unsubscribe()
   }
 }
